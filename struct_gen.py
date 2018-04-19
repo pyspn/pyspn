33d8f16b359b76104a406d5724b0c8929213055b
Pranav Subramani <p3subram@edu.uwaterloo.ca>
2018-04-19 03:02:16 -0400

    Added validity checking of the SPN

--- struct_gen.py
+++ struct_gen.py
@@ -4,6 +4,7 @@ import pdb
 import math
 from collections import defaultdict, deque
 
+
 class Scope(object):
     def __init__(self, x_ori, y_ori, x_size, y_size):
         self.x_ori = x_ori
@@ -15,30 +16,37 @@ class Scope(object):
         self.y_end = y_ori + y_size - 1
 
         total_coord = [x_ori, y_ori, x_size, y_size]
-        self.id = str(x_ori) + "_" + str(y_ori) + "_" + str(x_size) + "_" + str(y_size)
+        self.id = str(x_ori) + "_" + str(y_ori) + "_" + \
+            str(x_size) + "_" + str(y_size)
+
 
 class Leaf(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.depth = 0
-        self.id = str([x,y])
+        self.id = str([x, y])
+
 
 class Node(object):
-    def __init__(self, scope):
+    def __init__(self, scope, node_type=None):
         self.children = []
         self.scope = scope
         self.depth = 0
         self.id = None
+        self.node_type = node_type
+
 
 class Sum(Node):
-    def __init__(self, scope):
-        Node.__init__(self, scope)
+    def __init__(self, scope, node_type):
+        Node.__init__(self, scope, node_type)
         self.weights = []
 
+
 class Product(Node):
-    def __init__(self, scope):
-        Node.__init__(self, scope)
+    def __init__(self, scope, node_type):
+        Node.__init__(self, scope, node_type)
+
 
 class ConvSPN(object):
     def __init__(self, x_size, y_size, sum_shifts, prd_subdivs):
@@ -94,7 +102,7 @@ class ConvSPN(object):
         self.count_by_depth[depth] += 1
 
         # Generate root and all of its child products from shifts
-        root = Sum(scope)
+        root = Sum(scope, node_type="Sum")
         root.id = scope.id
         root.depth = depth
 
@@ -124,7 +132,7 @@ class ConvSPN(object):
         self.count_by_depth[depth] += 1
 
         # Generate root and all of its child sums from subdivisions
-        root = Product(scope)
+        root = Product(scope, node_type="Product")
         root.id = scope.id
         root.depth = depth
 
@@ -153,7 +161,6 @@ class ConvSPN(object):
 
     def generate_shifts(self, scope):
         # sum_shifts
-
         '''
         To determine:
         - what is a reasonable shift?
@@ -273,7 +280,8 @@ class ConvSPN(object):
 
             total_nodes += node_count
             total_edges += edge_count
-            print("Level " + str(level) + ": " + str(node_count) + " nodes, " + str(edge_count) + " edges")
+            print("Level " + str(level) + ": " + str(node_count) +
+                  " nodes, " + str(edge_count) + " edges")
             level += 1
 
         print(str(total_nodes) + " nodes")
@@ -300,9 +308,81 @@ class ConvSPN(object):
             print("Level " + str(level) + ": " + str(len(all)))
             level += 1
 
+
+def overlap(scope1, scope2):
+    '''
+    Two scopes overlap if they have some common elements between them
+    '''
+    scope1 = (int(scope1[0]), int(scope1[2]), int(scope1[4]), int(scope1[6]))
+    scope2 = (int(scope2[0]), int(scope2[2]), int(scope2[4]), int(scope2[6]))
+    x_ori_1 = scope1[0]  # xo1
+    x_end_1 = scope1[2]  # xe1
+    y_ori_1 = scope1[1]  # yo1
+    y_end_1 = scope1[3]  # ye1
+    x_ori_2 = scope2[0]  # xo2
+    x_end_2 = scope2[2]  # xe2
+    y_ori_2 = scope2[1]  # yo2
+    y_end_2 = scope2[3]  # ye2
+    # Case 1: (xo1 <= xo2 <= xe1) && (yo1 <= yo2 <= ye1)
+    if ((x_ori_1 <= x_ori_2 <= x_end_1) and (y_ori_1 <= y_ori_2 <= y_end_1)) or ((x_ori_2 <= x_ori_1 <= x_end_2) and (y_ori_2 <= y_ori_1 <= y_end_2)):
+        return True
+    # Case 2: (xo1 <= xe2 <= xe1) && (yo1 <= y02 <= ye1)
+    elif ((x_ori_1 <= x_end_2 <= x_end_1) and (y_ori_1 <= y_ori_2 <= y_end_1)) or ((x_ori_2 <= x_end_1 <= x_end_2) and (y_ori_2 <= y_ori_1 <= y_end_2)):
+        return True
+    # Case 3: (xo1 <= xo2 <= xe1) && (yo1 <= ye2 <= ye1)
+    elif ((x_ori_1 <= x_ori_2 <= x_end_1) and (y_ori_1 <= y_end_2 <= y_end_1)) or ((x_ori_2 <= x_ori_1 <= x_end_2) and (y_ori_2 <= y_end_1 <= y_end_2)):
+        return True
+    # Case 4: (xo1 <= xe2 <= xe1) && (yo1 <= ye2 <= ye1)
+    elif ((x_ori_1 <= x_end_2 <= x_end_1) and (y_ori_1 <= y_end_2 <= y_end_1)) or ((x_ori_2 <= x_end_1 <= x_end_2) and (y_ori_2 <= y_end_1 <= y_end_2)):
+        return True
+    else:
+        return False
+
+
+def check_validity(root):
+    '''
+    checks the consistency and completeness of the SPN
+    returns a tuple of id and True/False
+    '''
+    scope_hash = set()
+    for child in root.children:
+        if isinstance(child, Leaf):
+            if len(scope_hash) == 0:
+                scope_hash.add(child.id)
+            if root.node_type == 'Sum':
+                if child.id not in scope_hash:
+                    return -1, False
+                else:
+                    # prod
+                    if child.id in scope_hash:
+                        pdb.set_trace()
+                        return -1, False
+                    else:
+                        scope_hash.add(child.id)
+        else:
+            child_id, valid_flag = check_validity(child)
+            if valid_flag:
+                if len(scope_hash) == 0:
+                    scope_hash.add(child_id)
+                if root.node_type == 'Sum':
+                    if child_id not in scope_hash:
+                        return -1, False
+                else:
+                    for scope in scope_hash:
+                        if overlap(child_id, scope):
+                            pdb.set_trace()
+                            return -1, False
+                        else:
+                            scope_hash.add(child_id)
+            else:
+                return (-1, False)
+    return root.id, True
+
+
 cv = ConvSPN(32, 32, 8, 2)
 cv.generate_spn()
-
+root_id, flag = check_validity(cv.root)
+print(flag)
 cv.print_stat()
 
-pdb.set_trace()
+# pdb.set_trace()
