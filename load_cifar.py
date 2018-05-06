bb2fae570053dbbc9fddd13e46648df962f335b5
Kevin Daniel Pantasdo <kevin.pantasdo@uwaterloo.ca>
2018-05-05 22:57:30 -0400

    Multichannel sample generation

--- load_cifar.py
+++ load_cifar.py
@@ -17,12 +17,7 @@ from TorchSPN.src import network, param, nodes
 
 from tmm import *
 
-def main():
-    print("Loading models...")
-
-    model_name = 'mm_2_'
-    model = pickle.load(open(model_name, 'rb'))
-
+def save_activations(model, model_name):
     for (i, img_keys) in enumerate(model.networks):
         network = model.networks[img_keys]
         leaves = network.concat_leaves.child_list
@@ -35,15 +30,35 @@ def main():
         for (leaf_idx, leaf) in enumerate(leaves):
             leaf_channel = network.leaves_network_id[leaf_idx]
             input_index = network.leaves_input_indices[leaf_idx]
-            x_idx = int(input_index % structure.x_size)
-            y_idx = int(input_index / structure.x_size)
-            img[y_idx][x_idx][leaf_channel] = leaf.mean
+            img[input_index][leaf_channel] = leaf.mean
 
         img = (img + 0.5).clip(min=0, max=1)
 
         activation_name = "leaves_" + model_name + "_" + str(img_keys) + ".csv"
         np.savetxt(activation_name, img, delimiter=",")
         print("Activation saved as: " + activation_name)
+
+def generate_sample(model, model_name):
+    for img_keys in model.networks:
+        network = model.networks[img_keys]
+        img_size = network.structure.x_size * network.structure.y_size
+        n_var = img_size * 3
+        node2var = [network.leaves_network_id[i] * img_size + network.leaves_input_indices[i] for i in range(n_var)]
+        sample = model.GenSample(node2var, n_batch=1, n_var=n_var)[0]
+
+        img = (img + 0.5).clip(min=0, max=1).reshape((img_size, 3))
+
+        sample_name = "sample_" + model_name + "_" + str(img_keys) + ".csv"
+        np.savetxt(sample_name, img, delimiter=",")
+        print("Sample saved as: " + sample_name)
+
+def main():
+    print("Loading models...")
+
+    model_name = 'cifar_oneimg_2'
+    model = pickle.load(open(model_name, 'rb'))
+
+    generate_sample(model, model_name)
 
 if __name__=='__main__':
     main()
